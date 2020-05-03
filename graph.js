f54ebc28578498d495c10bb9daaedc697bf574e1
MostafaElGamal <mosta1598@gmail.com>
2020-05-03 22:26:51 +0200

    28

--- graph.js
+++ graph.js
@@ -26,18 +26,88 @@ class Graph {
     }
     delete this.adjacencyList[vertexName];
   }
+  depthFirstRecursive(start) {
+    const result = [];
+    const visited = {};
+    const adjacencyList = this.adjacencyList;
+
+    (function dfs(vertex) {
+      if (!vertex) return null;
+      visited[vertex] = true;
+      result.push(vertex);
+      adjacencyList[vertex].forEach((neighbor) => {
+        if (!visited[neighbor]) {
+          return dfs(neighbor);
+        }
+      });
+    })(start);
+
+    console.log(result);
+    return result;
+  }
+
+  depthFirstIterative(start) {
+    let stack = [];
+    let results = [];
+    let visited = {};
+    stack.push(start);
+    visited[start] = true;
+    while (stack.length >= 1) {
+      var currentStack = stack.pop();
+      results.push(currentStack);
+      this.adjacencyList[currentStack].forEach((neighbor) => {
+        if (!visited[neighbor]) {
+          visited[neighbor] = true;
+          stack.push(neighbor);
+        }
+      });
+    }
+    console.log(results);
+    return results;
+  }
+  breadthFirst(start) {
+    let queue, results, visited, currentVertex;
+    queue = [start];
+    results = [];
+    visited = {};
+    visited[start] = true;
+    while (queue.length) {
+      currentVertex = queue.shift();
+      results.push(currentVertex);
+      this.adjacencyList[currentVertex].forEach((neighbor) => {
+        if (!visited[neighbor]) {
+          visited[neighbor] = true;
+          queue.push(neighbor);
+        }
+      });
+    }
+    console.log(results);
+    return results;
+  }
 }
 
 var g = new Graph();
 
-g.addVertex("Egypt");
-g.addVertex("US");
-g.addVertex("Tokyo");
+g.addVertex("A");
+g.addVertex("B");
+g.addVertex("C");
+g.addVertex("D");
+g.addVertex("E");
+g.addVertex("F");
 
-g.addEdge("Egypt", "US");
-g.addEdge("Egypt", "Tokyo");
+g.addEdge("A", "B");
+g.addEdge("A", "C");
+g.addEdge("B", "D");
+g.addEdge("C", "E");
+g.addEdge("D", "E");
+g.addEdge("D", "F");
+g.addEdge("E", "F");
+g.breadthFirst("A");
 
-// g.removeEdge("Egypt", "US");
-g.removeVertex("US");
-
-console.log(g.adjacencyList);
+//          A
+//        /   \
+//       B     C
+//       |     |
+//       D --- E
+//        \   /
+//          F
